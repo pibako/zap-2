72653693b2f0b901bf2c34872248c07ce4d44ab2
Piotr Kowalski <piotrek.kowalski@gmail.com>
2012-03-17 19:10:18 +0100

    Fix spacing in list.h

--- Sample/list.h
+++ Sample/list.h
@@ -3,23 +3,27 @@
 
 /**
  * Lista jednokierunkowa; head wskazuje na pierwszy element listy
-*/
-
+ */
 struct Node {
     double value;
     Node* next;
 };
 
+/**
+ * ...
+ */
 struct List {
     Node* head; /* pierwszy element listy*/
 };
+
 /**
  * Funkcja tworzy pusta liste
-*/
+ */
 List* createList();
 
 /**
-*/
+ * ...
+ */
 void appendToList(List* list, double d);
 
 #endif _LIST_H_
